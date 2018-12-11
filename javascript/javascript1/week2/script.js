694882d4fa9acf16ff72dac7a552e7332a058302
samarasallam <39006846+samarasallam@users.noreply.github.com>
2018-12-11 13:16:25 +0100

    week2 js

---
+++ javascript/javascript1/week2/script.js
@@ -0,0 +1,35 @@
+var myLang = (" Hello , Hej, Salut");
+console.log(myLang); 
+
+var step2 = Math.round( 7.25);
+console.log(step2);
+
+var myArry = [];
+console.log(myArry=40);
+
+
+var favAnimal = ["snail"];
+favAnimal.push("baby big");
+console.log(favAnimal);
+
+
+let myString = "this is a test";
+var myLenght= myString.length;
+console.log(myLenght);
+
+
+var name =("Samara");
+var age =(27);
+var city=("Odense");
+var favFood = ["rice"," and more rice"];
+
+console.log('ma name is ' + name);
+console.log('im '+ age +' years old');
+console.log('i live in ' + city);
+console.log('ma favourite food is '+ favFood);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof favFood);
+console.log(typeof obj);
+
